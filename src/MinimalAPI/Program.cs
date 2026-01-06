f70d4d210c6db10121a84edb19ab75bba62de8c6
Carlos Garibay <garibaycarlos@live.com>
2026-01-06 15:26:48 -0600

    Create first endpoints: GET (all), GET (by id) and POST (add new.)

--- src/MinimalAPI/Program.cs
+++ src/MinimalAPI/Program.cs
@@ -1,6 +1,40 @@
+using MinimalAPI.Models;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var products = new List<Product> {
+    new() {Id = 1, Name = "Smart Phone" },
+    new() { Id = 2, Name = "Smart TV"}
+};
+
+app.MapGet("/products", async (HttpContext context) =>
+{
+    await context.Response.WriteAsync(JsonSerializer.Serialize(products));
+});
+
+app.MapGet("/products/{id:int}", async (HttpContext context, int id) =>
+{
+    var product = products.FirstOrDefault(p => p.Id == id);
+   
+    if (product is null)
+    {
+        context.Response.StatusCode = 404;
+    
+        await context.Response.WriteAsync("Product not found");
+        
+        return;
+    }
+    
+    await context.Response.WriteAsync(JsonSerializer.Serialize(product));
+});
+
+app.MapPost("/products", async (HttpContext context, Product product) =>
+{
+    products.Add(product);
+
+    await context.Response.WriteAsync("Product added");
+});
 
 app.Run();
