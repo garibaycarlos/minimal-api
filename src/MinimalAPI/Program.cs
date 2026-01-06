14ac2c5f9de406a240defdb707af5570970cf9fd
Carlos Garibay <garibaycarlos@live.com>
2026-01-06 16:56:14 -0600

    01. Add RouteGroupBuilder class as an extension method. 02. Move endpoints from Program class to RouteGroupBuilder and use MapGroup. 03. Add PUT and DELETE endpoints.

--- src/MinimalAPI/Program.cs
+++ src/MinimalAPI/Program.cs
@@ -1,40 +1,8 @@
-using MinimalAPI.Models;
-using System.Text.Json;
+using MinimalAPI.RouteGroups;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var products = new List<Product> {
-    new() {Id = 1, Name = "Smart Phone" },
-    new() { Id = 2, Name = "Smart TV"}
-};
-
-app.MapGet("/products", async (HttpContext context) =>
-{
-    await context.Response.WriteAsync(JsonSerializer.Serialize(products));
-});
-
-app.MapGet("/products/{id:int}", async (HttpContext context, int id) =>
-{
-    var product = products.FirstOrDefault(p => p.Id == id);
-   
-    if (product is null)
-    {
-        context.Response.StatusCode = 404;
-    
-        await context.Response.WriteAsync("Product not found");
-        
-        return;
-    }
-    
-    await context.Response.WriteAsync(JsonSerializer.Serialize(product));
-});
-
-app.MapPost("/products", async (HttpContext context, Product product) =>
-{
-    products.Add(product);
-
-    await context.Response.WriteAsync("Product added");
-});
+app.MapGroup("/products").ProductsAPI();
 
 app.Run();
---
+++ src/MinimalAPI/RouteGroups/ProductsMapGroup.cs
@@ -0,0 +1,82 @@
+﻿using MinimalAPI.Models;
+using System.Text.Json;
+
+namespace MinimalAPI.RouteGroups;
+
+public static class ProductsMapGroup
+{
+    private static List<Product> products = new () {
+    new() {Id = 1, Name = "Smart Phone" },
+    new() { Id = 2, Name = "Smart TV"}
+};
+
+    public static RouteGroupBuilder ProductsAPI(this RouteGroupBuilder group)
+    {
+
+        group.MapGet("/", async (HttpContext context) =>
+        {
+            await context.Response.WriteAsync(JsonSerializer.Serialize(products));
+        });
+
+        group.MapGet("/{id:int}", async (HttpContext context, int id) =>
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            if (product is null)
+            {
+                context.Response.StatusCode = 404;
+
+                await context.Response.WriteAsync("Product not found");
+
+                return;
+            }
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(product));
+        });
+
+        group.MapPost("/", async (HttpContext context, Product product) =>
+        {
+            products.Add(product);
+
+            await context.Response.WriteAsync("Product added");
+        });
+
+        group.MapPut("/{id:int}", async (HttpContext context, int id, Product updatedProduct) =>
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            
+            if (product is null)
+            {
+                context.Response.StatusCode = 404;
+
+                await context.Response.WriteAsync("Product not found");
+                
+                return;
+            }
+            
+            product.Name = updatedProduct.Name;
+            
+            await context.Response.WriteAsync("Product updated");
+        });
+
+        group.MapDelete("/{id:int}", async (HttpContext context, int id) =>
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            
+            if (product is null)
+            {
+                context.Response.StatusCode = 404;
+            
+                await context.Response.WriteAsync("Product not found");
+                
+                return;
+            }
+            
+            products.Remove(product);
+            
+            await context.Response.WriteAsync("Product deleted");
+        });
+
+        return group;
+    }
+}
