f930e7c10559558c64c186d6067e6e82f681f97d
Carlos Garibay <garibaycarlos@live.com>
2026-01-09 14:34:34 -0600

    Make model fields required and add range for Id field.

--- src/MinimalAPI/Models/Product.cs
+++ src/MinimalAPI/Models/Product.cs
@@ -1,8 +1,13 @@
-﻿namespace MinimalAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinimalAPI.Models;
 
 public class Product
 {
+    [Required (ErrorMessage = "Id cannot be blank")]
+    [Range(0, int.MaxValue, ErrorMessage = "Id must be between 0 and the maximum value of int")]
     public int Id { get; set; }
+    [Required(ErrorMessage = "Name cannot be blank")]
     public string Name { get; set; } = string.Empty;
 
     public override string ToString() => $"Product Id: {Id}, Product Name: {Name}";
